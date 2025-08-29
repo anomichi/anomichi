6730f06b48f94a98b454a1fb63572f7d212dd47f
Naoyuki Miyazaki <16092751+nao5151@users.noreply.github.com>
2025-08-29 23:51:19 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://anomichi.github.io/anomichi/',
+	base: '/anomichi/',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
