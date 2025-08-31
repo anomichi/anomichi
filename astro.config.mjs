eea376f8d234ee7984e1498e14c63a60c13ffdf1
Naoyuki Miyazaki <16092751+nao5151@users.noreply.github.com>
2025-08-31 15:01:25 +0900

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://anomichi.github.io/anomichi/',
-	base: '/anomichi/',
-	output: 'static',
-	integrations: [mdx(), sitemap()],
+  site: 'https://anomichi.github.io/blog/',
+  base: '/blog/',
+  output: 'static',
+  integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'あの日走った道の名前を僕達はまだ知らない。';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
